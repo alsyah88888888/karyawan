b789ffc6a7a626ab48683f6010bd8b6789307e35
alsyah88888888 <lubisr78@gmail.com>
2026-03-16 00:14:36 +0700

    fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'koboi-cache-v1';
+const CACHE_NAME = 'koboi-cache-v2';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -7,13 +7,28 @@ const urlsToCache = [
   '/style.css',
   '/script.js',
   '/portal.js',
-  '/images/koboi.png?v=2'
+  '/images/koboi.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
