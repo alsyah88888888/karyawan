e3588750ba097013a152aa44760eaeee2c236da1
alsyah88888888 <lubisr78@gmail.com>
2026-05-03 19:56:34 +0700

    fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'koboi-cache-v1';
+const CACHE_NAME = 'koboi-hris-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -7,24 +7,30 @@ const urlsToCache = [
   './employee.css',
   './script.js',
   './employee.js',
-  './logokoboi.png'
+  './logokoboi.png',
+  'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700;800;900&display=swap',
+  'https://unpkg.com/lucide@latest'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Fall back to network
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
