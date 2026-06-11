a4f1b561c17d8845ab692875a8b5f319dabf3652
alsyah88888888 <lubisr78@gmail.com>
2026-06-11 18:18:05 +0700

    fix

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJ
 const supabaseClient = supabase.createClient(SB_URL, SB_KEY);
 
 // Tambahkan IP baru ke dalam daftar (Array) agar lebih fleksibel
-const OFFICE_IPS = ["202.51.197.75 ", "103.108.130.43", "124.158.189.235", "114.124.238.252", "202.51.197.78"];
+const OFFICE_IPS = ["202.51.197.75", "103.108.130.43", "124.158.189.235", "114.124.238.252", "202.51.197.78"];
 let KARYAWAN = [];
 let allLogs = [];
 let bypassWiFi = false;
@@ -85,7 +85,8 @@ async function updateWiFiStatus() {
 
     const res = await fetch("https://api.ipify.org?format=json");
     const data = await res.json();
-    const isOffice = OFFICE_IPS.includes(data.ip) || bypassWiFi;
+    const currentIp = data.ip ? data.ip.trim() : "";
+    const isOffice = OFFICE_IPS.map(ip => ip.trim()).includes(currentIp) || bypassWiFi;
 
     if (isOffice) {
       badge.innerText = bypassWiFi ? "BYPASS MODE AKTIF ✅" : "WIFI KANTOR TERHUBUNG ✅";
